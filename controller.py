df183189ed85443355624930ee6e7831e3b82b29
PeterHLin <70664040+PeterHLin@users.noreply.github.com>
2022-02-05 16:18:21 -0500

    added window to game

--- controller.py
+++ controller.py
@@ -3,3 +3,7 @@ import pygame
 class Controller:
     def __init__(self):
         self.screen = pygame.display.set_mode((1000,1000))
+        self.gameState = "start"
+    def game(self):
+        while self.gameState == "start":
+            print("hello")
--- game.py
+++ game.py
@@ -1,6 +1,8 @@
 import pygame
 import controller
+
 def main():
     pygame.init()
     game = controller.Controller()
+    game.game()
 main()
