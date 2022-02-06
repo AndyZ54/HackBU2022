b382f6669beaba9cc8067a1d4c62d8a9b435dc4b
AndyZ54 <Andyzheng54@yahoo.com>
2022-02-06 04:17:44 -0500

    fixed obstacles

--- src/obstacles.py
+++ src/obstacles.py
@@ -1,20 +1,21 @@
 import pygame
+import random
 
 class Obstacles:
     #creates and initalizes the obstacle position
-    def__init__(self):
+    def __init__(self):
         #selects random obstacle
         obstacleNum = random.randrange(1,5)
         if obstacleNum == 1:
-            self.image = pygame.image.load('assets/obstacle1.png')
+            self.image = pygame.image.load('assets/obstacles/obstacle1.png')
         elif obstacleNum == 2:
-            self.image = pygame.image.load('assets/obstacle2.png')
+            self.image = pygame.image.load('assets/obstacles/obstacle2.png')
         elif obstacleNum == 3:
-            self.image = pygame.image.load('assets/obstacle3.png')
+            self.image = pygame.image.load('assets/obstacles/obstacle3.png')
         elif obstacleNum == 4:
-            self.image = pygame.image.load('assets/obstacle4.png')
+            self.image = pygame.image.load('assets/obstacles/obstacle4.png')
         elif obstacleNum == 5:
-            self.image = pygame.image.load('assets/obstacle5.png')
+            self.image = pygame.image.load('assets/obstacles/obstacle5.png')
         self.rect = self.image.get_rect()
         self.rect.x = 1600
 
@@ -26,5 +27,5 @@ class Obstacles:
             self.obstacles.pop()
 
     #draw the object
-    def draw(self, pygame.display.set_mode((1600,900))):
+    def draw(self, screen):
         screen.blit(self.image, self.rect())
