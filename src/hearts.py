6136b2ff43c46e31aecfeaf11e8bc9306a7bd5fd
PeterHLin <70664040+PeterHLin@users.noreply.github.com>
2022-02-06 05:30:12 -0500

    a

--- src/hearts.py
+++ src/hearts.py
@@ -9,6 +9,7 @@ class Hearts(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+<<<<<<< Updated upstream
         self.update()
     def update(self):
         self.rect.x -= 10
@@ -16,3 +17,8 @@ class Hearts(pygame.sprite.Sprite):
             self.kill()
     def draw(self, screen):
         screen.blit(self.image, (self.rect.x, self.rect.y))
+=======
+        self.update_x()
+    def update_x(self):
+        self.rect.x -= 15
+>>>>>>> Stashed changes
